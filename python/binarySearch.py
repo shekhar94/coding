33fb208ed21539155644e7449f76b832398119b1
SHEKHAR\shekharsuman <shekharsumannit90@gmail.com>
2018-05-12 11:27:33 +0530

    ctci-ice-cream-parlor

--- python/binarySearch.py
+++ python/binarySearch.py
@@ -29,6 +29,9 @@
 
 # main()
 
+
+#  https://www.hackerrank.com/challenges/ctci-ice-cream-parlor/problem 
+#  Author: Shekhar Suman 
 class IceCream:
     def __init__(self, id, cost):
         self.id = id
@@ -37,9 +40,58 @@ class IceCream:
 def get_ice_cream_object_arr(costArr):
     costIdArr = list()
     i = 0
-    for i in range(len(costArr))
+    for i in range(len(costArr)):
         costIdArr.append(IceCream(i + 1, costArr[i]))
     return costIdArr
-def make_comparator()
+def comparator(cost1, cost2):
+    if cost1 > cost2:
+        return 1
+    elif cost1 < cost2:
+        return -1
+    else:
+        return 0
 def sort_ice_cream_obj_arr(costIdArr):
-    sortedCostIdArr = sorted(costIdArr, cmp=make_comparator(cmpVal))
+    sortedCostIdArr = sorted(costIdArr, key=lambda iceCream: iceCream.cost)
+    print (sortedCostIdArr)
+    return sortedCostIdArr
+
+def print_result(sortedCostIdArr, i, j):
+    if (sortedCostIdArr[i].id < sortedCostIdArr[j].id):
+        print (sortedCostIdArr[i].id, sortedCostIdArr[j].id)
+    else:
+        print (sortedCostIdArr[j].id, sortedCostIdArr[i].id)
+
+
+def binary_search(arr, num, left, right):
+    if left > right:
+        return -1
+    mid = (left + right) // 2
+    if arr[mid].cost == num:
+        return mid
+    elif arr[mid].cost > num:
+        return binary_search(arr, num, left, mid - 1)
+    else:
+        return binary_search(arr, num, mid + 1, right)
+
+def find_ice_creams(noOfFlavor, sortedCostIdArr, money):
+    isMatchFound = False
+    i = 0
+    while (isMatchFound is False) & (i < noOfFlavor):
+        cost1 = sortedCostIdArr[i].cost
+        cost2 = money - cost1
+        j = binary_search(sortedCostIdArr, cost2, i + 1, noOfFlavor - 1)
+        if j != -1:
+            print_result(sortedCostIdArr, i, j)
+            isMatchFound = True
+        i += 1
+
+def main():
+    money = 4
+    noOfFlavor = 5
+    costArr = [1,4,5,3,2]
+    costIdArr = get_ice_cream_object_arr(costArr)
+    sortedCostIdArr = sort_ice_cream_obj_arr(costIdArr)
+    find_ice_creams(noOfFlavor, sortedCostIdArr, money)
+
+main()
+
