b6c0812e875215654b8d97a7ae572ce482d17046
Shekhar <shekharsuman@triconinfotech.com>
2018-05-18 21:02:40 +0530

    c++ implementation for dfs and bfs

---
+++ cpp/graphSearch.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+class Node {
+    private:
+    int id;
+    vector<Node> adjacent()
+
+}
+// int main() {
+//     vector<int>  v1(10);
+//     for (int i = 0; i < 10; i++) {
+//         cout<<v1[i];
+//     }
+//     cout << "Hello world";
+// }
