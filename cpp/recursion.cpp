2734ecf93702fbace0976b4b130bf529a5a061bf
Shekhar <shekharsuman@triconinfotech.com>
2018-05-29 21:10:39 +0530

    c++

--- cpp/recursion.cpp
+++ cpp/recursion.cpp
@@ -19,18 +19,21 @@ int find_number_of_ways(int n, map<int, int> &count_map, map<pair<int, int>, int
         c_1 = it_j->second;
     } else {
         c_1 = find_number_of_ways(n - 1, count_map, count_jump_map);
+        count_jump_map.insert(make_pair<make_pair<int, int>, int> ((n, 1), c_1);
     }
     it_j = count_jump_map.find(pair<int, int> (n, 2));
     if (it_j != count_jump_map.end()) {
         c_2 = it_j->second;
     } else {
         c_2 = find_number_of_ways(n - 2, count_map, count_jump_map);
+        count_jump_map.insert(make_pair<make_pair<int, int>, int> ((n, 2), c_2);
     }
     it_j = count_jump_map.find(pair<int, int> (n, 3));
     if (it_j != count_jump_map.end()) {
         c_3 = it_j->second;
     } else {
         c_3 = find_number_of_ways(n - 3, count_map, count_jump_map);
+        count_jump_map.insert(make_pair<make_pair<int, int>, int> ((n, 3), c_3);
     }
     count += c_1 + c_2 + c_3;
     return count;
