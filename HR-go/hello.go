bc63f3e4174aa8938cb7c29de113edee6fd474dd
SHEKHAR\shekharsuman <shekharsumannit90@gmail.com>
2018-06-10 00:48:16 +0530

    hello world in go lang

---
+++ HR-go/hello.go
@@ -0,0 +1,12 @@
+// building go program
+// build single file 
+// > go build -o output_file_name.exe input_file_name.go
+// build complete package 
+// > go build (builds with directory_name.exe as output file)
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello, world\n")
+}
