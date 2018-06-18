f839df39126fffeb31a4b9f0712362c947281fd7
Shekhar <shekharsuman@triconinfotech.com>
2018-06-18 12:13:03 +0530

    -new addition

--- HR-js/equal-dp.js
+++ HR-js/equal-dp.js
@@ -4,7 +4,7 @@ function main(input) {
     let ip_arr = input.split('\n');
     let no_of_colleagues = Number(ip_arr[0]);
     let choco_arr = ip_arr[1].split(' ').map(Number);
-
+    findMinOperations(choco_arr, [1, 3, 5], 0, 0);
 }
 
 function getUniqueArr(choco_arr) {
@@ -19,15 +19,15 @@ function getUniqueArr(choco_arr) {
     return unique_choco_arr;
 }
 
-function findMinOperations(choco_arr) {
+function findMinOperations(choco_arr, distribution_arr, left_out_friend_index, dist_index) {
     // can  give 1,3,5 choco at a time
     let operations_count = 0;
     if (choco_arr.length === 1) {
         return 0;
     }
-    let unique_choco_arr = getUniqueArr(choco_arr);
-    for (let i = 0; i < unique_choco_arr.length; i++) {
-
-    }
+    let unique_choco_arr = getUniqueArr(choco_arr, distribution_arr);
+    operations_count++;
+    operations_count = findMinOperations(unique_choco_arr, distribution_arr, left_out_friend_index, dist_index);
+    return operations_count;
 }
 main("4\n2 2 3 7");
