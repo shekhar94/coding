e75327db10485261b844a1561a865a902f9a059d
shekhar <shekhar.suman@cybernetyx.com>
2019-08-19 08:43:34 +0530

    - lazy loading

--- angular/customer-app/src/app/app-routing.module.ts
+++ angular/customer-app/src/app/app-routing.module.ts
@@ -9,6 +9,10 @@ respectively. Notice that the lazy loading
 syntax uses loadChildren followed by a
 function that uses the browser's built-in
 import('...') syntax for dynamic imports. The import path is the relative path to the module.
+
+for running in production
+ng serve --configuration=production
+ng serve (0.js, 1.js are chunks for customers and orders routes)
 */
 const routes: Routes = [
   {
