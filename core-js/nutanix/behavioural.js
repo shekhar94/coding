139b365d6ff1434f47e4105c017cd6a75ee761bf
shekhar <shekhar.suman@cybernetyx.com>
2019-09-09 19:32:32 +0530

    behavioural

--- core-js/nutanix/behavioural.js
+++ core-js/nutanix/behavioural.js
@@ -7,3 +7,17 @@
 1. Understand your worth wrt time(you are capable of more)
 2. Cross-team connection  (dada ke sath) (Hyper emotions)
 */
+
+/* 
+1. Dedication never loose hope(pimpari), team player
+2. Attention to detail(nextv)
+3. Handles stress well(catfish)
+4. Fast learner
+
+1. Communication 
+2. 
+3. 
+
+*/
+
+
