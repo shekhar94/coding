7bb927d459dc929c95adf2ca8ce3a98d97da0537
shekhar <shekhar.suman@cybernetyx.com>
2019-09-09 19:26:38 +0530

    - behavioural questions

---
+++ core-js/nutanix/behavioural.js
@@ -0,0 +1,9 @@
+/*
+1. Extra mile
+2. Hungry for learning new things
+3. Confident about his knowledge
+
+
+1. Understand your worth wrt time(you are capable of more)
+2. Cross-team connection  (dada ke sath) (Hyper emotions)
+*/
