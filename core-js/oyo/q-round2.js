e821be2b186730a6276ba7004a2dcc3afbb5cb50
shekhar <shekhar@mahakal.com>
2019-09-28 22:44:34 +0530

    oyo round 2

---
+++ core-js/oyo/q-round2.js
@@ -0,0 +1,14 @@
+/* 
+1. Given a string "ADBCBA". 
+Find the length of longest palindromic sub-sequence.
+For "ADBCBA" longest sub-sequence is ABCBA and length is 5.
+Hint: Dynamic programming build in bottom up fashion.
+Try to find sub-sequence of length 1 then 2 then 3
+and so on. 
+
+
+2. Given k sorted arrays each of size n. Print numbers 
+in ascending order by merging all of them. Time complexity
+should be of order of (k*n*logn).
+Hint: Use min heap
+*/
