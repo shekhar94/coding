756eb4de1d02a64a454423ebb41b7877ccbec3a5
shekhar <shekhar@mahakal.com>
2019-09-28 22:23:45 +0530

    oyo round1

---
+++ core-js/oyo/q-round1.js
@@ -0,0 +1,24 @@
+/* 
+1. Given matrix
+[
+    [1,2,3,4,5],
+    [1,2,3,4,5],
+    [1,2,3,4,5],
+    [1,2,3,4,5]
+]
+Let's assume you have two pointers (a,b) and (c,d) (c>a, d>b)pointing to
+two different entries in two-d array. Find the sum of all the 
+elements which are part of the rectangle created by these two points.
+Optimize time complexity to O(n). Preprocessing on the array elements 
+is allowed. n*m space is also allowed.
+
+2. Design database schema for book-my-show
+Users management
+Cities
+Movies
+Ticket Booking
+Tracking seat availability
+Show list of movies based on search. Show list of theaters where
+this movie is running
+Optimise search query
+*/
