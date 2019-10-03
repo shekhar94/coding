24091a8170f126f756d434dc57b2db97a760d8dd
shekhar94 <shekhar.suman@nutanix.com>
2019-10-03 09:56:56 +0530

    spring boot learning docs

---
+++ spring-boot/docs/doc.java
@@ -0,0 +1,13 @@
+/* 
+Courses TODO:
+1. Reactive programming with java8
+2. Reactive Spring
+
+shortcuts
+cmd+n
+cmd+alt+l to format
+cmd+shift+t to generate unit test file
+
+WebFlux
+FlatMap in java
+ */
