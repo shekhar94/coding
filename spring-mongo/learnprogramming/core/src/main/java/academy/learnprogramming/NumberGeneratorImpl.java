a78ad4ffed4bf3ee49300feda233f0baee482654
shekhar94 <shekhar.suman@nutanix.com>
2020-07-28 14:02:46 +0530

    sprint masterclass

---
+++ spring-mongo/learnprogramming/core/src/main/java/academy/learnprogramming/NumberGeneratorImpl.java
@@ -0,0 +1,31 @@
+package academy.learnprogramming;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+@Getter
+@Component
+public class NumberGeneratorImpl implements NumberGenerator {
+
+    @Getter(AccessLevel.NONE)
+    private final Random random = new Random();
+
+    private final int maxNumber;
+
+    private final int minNumber;
+
+    @Autowired
+    public NumberGeneratorImpl(@MaxNumber int maxNumber, @MinNumber int minNumber) {
+        this.maxNumber = maxNumber;
+        this.minNumber = minNumber;
+    }
+
+    @Override public int next() {
+        return random.nextInt(maxNumber - minNumber) + minNumber;
+    }
+
+}
