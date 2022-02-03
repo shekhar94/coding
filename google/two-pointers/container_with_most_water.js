20f05a925449a29188821cc6e047b0db8752ea6c
shekhar94 <shekhar.suman@nutanix.com>
2022-02-03 10:24:52 +0530

    https://leetcode.com/problems/container-with-most-water/

---
+++ google/two-pointers/container_with_most_water.js
@@ -0,0 +1,23 @@
+function maxArea(arr) {
+    let s = 0;
+    let e = arr.length - 1;
+    let mArea = 0;
+
+    while (s < e) {
+        const area = Math.min(arr[s], arr[e]) * (e - s);
+        if (area > mArea) mArea = area;
+        if (arr[s] < arr[e]) s++;
+        else e--;
+    }
+    return mArea;
+}
+
+function main() {
+    // const arr = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+    // const arr = [1, 1];
+    // const arr = [1, 5, 4, 3];
+    const arr = [3, 1, 2, 4, 5];
+    console.log(maxArea(arr));
+}
+
+main();
