888be2b19b3f6068d3968afa005454475f941182
shekhar94 <shekhar.suman@nutanix.com>
2022-02-06 15:37:10 +0530

    https://leetcode.com/problems/happy-number/submissions/

--- google/hash-map/happy-number.js
+++ google/hash-map/happy-number.js
@@ -2,25 +2,21 @@
 function isHappy(n) {
     const map = new Map();
     map.set(n, true);
-    while (true) {
+    while (n != 1) {
         const sum = n.toString()
             .split('')
-            .map(num => parseInt(num))
-            .reduce((acc, num) => {
-                acc += num ** 2
-                return acc;
-            }, 0);
+            .reduce((acc, num) => acc + num ** 2, 0);
 
-        if (sum === 1) return true;
         if (map.has(sum)) return false;
         else map.set(sum, true);
         n = sum;
     }
+    return true;
 }
 
 function main() {
-    // const n = 19;
-    const n = 2;
+    const n = 19;
+    // const n = 2;
     console.log(isHappy(n));
 }
 
